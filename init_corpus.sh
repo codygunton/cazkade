0668488db661b4fefcdb7739e58f983831fdb98e
Cody Gunton <codygunton@gmail.com>
2025-07-06 10:51:18 -0400

    mv instead of cp

--- init_corpus.sh
+++ init_corpus.sh
@@ -9,4 +9,4 @@ source cascade-meta/env.sh
 tmp=$(mktemp -d)
 CASCADE_JOBS=48 python3 cascade-meta/fuzzer/do_genmanyelfs.py 8192 $tmp
 mkdir -p corpus
-cp $tmp/risc0_*.elf corpus/
+mv $tmp/risc0_*.elf corpus/
